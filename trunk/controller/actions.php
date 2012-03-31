1e06b4d5e8adba320ea13c76a5938af9f045ee61
nathanael.couret@gmail.com <nathanael.couret@gmail.com@03b04cd0-f2ea-658a-dc89-4b6bc1b6f185>
2012-03-31 22:46:17 +0000

    Ataw - modèle MVC fonctionnel, quelques corrections à effectuer mais résultats satisfaisants.

---
+++ trunk/controller/actions.php
@@ -0,0 +1,69 @@
+<?php
+	session_start();
+	error_reporting(E_ALL | E_STRICT );
+	ini_set('display_errors', true);
+	
+	include_once "../model/connect.php";
+	include_once "blagues.php";
+	if(isset($_POST["action"])){
+		if(($_POST["action"]=="ajouter")){
+			if(isset($_SESSION["pseudo"]))
+				addBlague($_POST["texte"], $_SESSION["pseudo"], $_POST["categorie"]);
+		}
+		else if(($_POST["action"]=="supprimer")){
+			deleteBlague($_POST["categorie"]);		
+		}
+		else if(($_POST["action"]=="Jaime")){
+			$query = "update blagues set nbJaime = nbJaime+1 where id='".$_POST["id"]."'";
+			mysql_query($query) or die(mysql_error());
+			$query = "update synchro_jaime_log set jaime = 1 where id_blague='".$_POST["id"]."' and id_log='".$_SESSION["pseudo"]."'";
+			$result = mysql_query($query) or die(mysql_error());
+			if(mysql_affected_rows() == 0)
+			{
+				$query = "insert into synchro_jaime_log(id_log,id_blague,jaime) values('".$_SESSION["pseudo"]."','".$_POST["id"]."',1)";
+				$result = mysql_query($query) or die(mysql_error());
+			}
+		}
+		else if(($_POST["action"]=="Jaime plus")){
+			$query = "update blagues set nbJaime = nbJaime-1 where id='".$_POST["id"]."'";
+			mysql_query($query) or die(mysql_error());
+			$query = "update synchro_jaime_log set jaime = 0 where id_blague='".$_POST["id"]."' and id_log='".$_SESSION["pseudo"]."'";
+			mysql_query($query) or die(mysql_error());
+		}
+		else if(($_POST["action"]=="Commenter")){
+			$query = "insert into com(id_log,id_blague,commentaire) values('".$_SESSION['pseudo']."','".$_POST['id']."','".$_POST['commentaire']."')";
+			mysql_query($query) or die(mysql_error());
+		}
+		else if(($_POST["action"]=="Connexion")){
+			if ( checkLogin($_POST["pseudo"], $_POST["mdp"])){
+				unset($_SESSION['erreur_connect']);
+				$_SESSION["pseudo"] = $_POST["pseudo"];
+				$query = "select admin from log where login='".$_SESSION["pseudo"]."'";
+				$result = mysql_query($query);
+				$row = mysql_fetch_assoc($result);
+				$_SESSION["admin"] = $row['admin'];
+			}
+		}
+		else if(($_POST["action"]=="Deconnexion")){
+			unset($_SESSION["pseudo"]);
+		}
+		else if(($_POST["action"]=="Inscription")){
+			echo '<script language="Javascript">document.location.replace("../view/inscription.php");</script>';
+		}
+		else if(($_POST["action"]=="Sinscrire")){
+			$query = "select * from log where login='".$_POST["pseudo"]."'";
+			$result=mysql_query($query);
+			if(mysql_num_rows($result) == 0)
+			{
+				addPseudo($_POST["pseudo"], $_POST["mdp"], $_POST["mail"], $_POST["naissance"]);
+			}
+			else
+			{
+				$_SESSION["erreur_inscrip"] = "pseudo existe deja";
+				echo '<script language="Javascript">document.location.replace("../view/inscription.php");</script>';
+			}
+		}
+
+	}
+	echo '<script language="Javascript">document.location.replace("../.");</script>';
+?>
--- trunk/index.php
+++ trunk/index.php
@@ -1,13 +1,54 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+session_start(); 
+include_once "model/connect.php";
+include_once "controller/blagues.php";
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta http_equiv="Author" content="Ataw" />
-        <title>Document sans titre</title>
-    </head>
-    <body>
-        <div id="wrapper">
-        <?php echo $content->getContent(); ?>
-        </div>
-    </body>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Blagues carambar</title>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<script type="text/javascript" src="js/verification.js"></script>
+<script type="text/javascript" src="js/calendrier.js"></script>
+</head>
+<body>
+
+<div id="all">
+
+<div id="header">
+<?php
+displayAddFormLog();
+?>
+<h1>Blagues</h1>
+</div>
+<div id="content">
+
+<div id="blagues">
+<h2>Toutes les blagues</h2>
+<?php 
+displayBlagues();
+?>
+</div>
+
+
+<div id="addblague">
+<h2>Ajouter une blague</h2>
+<?php 
+displayAddForm();
+?>
+</div>
+
+<div id="deleteblague">
+<h2>Supprimer une blague</h2>
+<?php 
+displayDeleteForm();
+?>
+</div>
+
+</div><!-- content -->
+
+</div><!-- all -->
+
+</body>
 </html>
