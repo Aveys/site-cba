fac24475b51074ef81a130a79376248d0bee7100
mathieumartin743@gmail.com <mathieumartin743@gmail.com@03b04cd0-f2ea-658a-dc89-4b6bc1b6f185>
2012-04-02 14:20:18 +0000

    Fichier des variables global

---
+++ stul_config.php
@@ -0,0 +1,13 @@
+<?php
+//Ce fichier contient tout les variables globales que nous avons besoin
+//Une variable qui commence par $f sera un fichier
+
+
+$theme = "themes/cba/";
+
+//Chemin des fichiers
+$fActionPhp = "actions.php";
+$fAdminFonct = "stul_adminfonct.php";
+
+
+?>
