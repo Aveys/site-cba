a598059a166d7b6faf928452b6a04d21c469a7af
thomas.rovayaz74@gmail.com <thomas.rovayaz74@gmail.com@03b04cd0-f2ea-658a-dc89-4b6bc1b6f185>
2012-04-02 21:36:24 +0000

    fonction d'affichage rediriger vers user_view.php

--- trunk/profil.php
+++ trunk/profil.php
@@ -1,7 +1,8 @@
 <?php
 	session_start();
-	include_once "connect.php";
+	include_once "user_view.php";
 	include_once "articles.php";
+	include_once "includes/admin/stul_fonctions.php";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -25,31 +26,6 @@
 		editer_info_user($_GET['id']);
 	else
 		afficher_info_user($_GET['id']);
-
-	function afficher_info_user($id)
-	{
-		if(autorise_edition($id))
-			echo "<a href="."profil.php?id=".$id."&editer=1>editer</a></br>";
-		$row = sql_info_user($id);
-		echo "Mail: ".$row['user_mail']."</br>";
-		//affiche_anni($row['date_naissance'],$login);
-	}
-
-	function editer_info_user($id)
-	{
-		if(autorise_edition($id))
-		{
-			$row = sql_info_user($id);
-			echo "</br></br><form method='post' name='editer_user' action='actions.php'>";
-				echo "<label for='mail'>Mail :</label>";
-				echo "<input name='mail' value='".$row['user_mail']."'/></br></br>";
-				//echo "<label for='naissance'>Date de naissance :</label>";
-				//echo "<input type='text' name='naissance' class='calendrier' size='8' value='".$row['date_naissance']."'/></br></br>";
-				echo "<input type='submit' name='action' value='Editer'/>";
-				echo "<input type='hidden' name='id' value='".$id."'/>";
-			echo"</form>";
-		}
-	}
 	function autorise_edition($id)
 	{
 		if(isset($_SESSION['id']) && (isadmin($_SESSION['id']) || $id == $_SESSION['id']))
