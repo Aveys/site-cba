8b2e5fa5f1419c0f9753d0d21b8bdc93d6ac9f29
nathanael.couret@gmail.com <nathanael.couret@gmail.com@03b04cd0-f2ea-658a-dc89-4b6bc1b6f185>
2012-04-02 12:08:13 +0000

    test POO pour users

--- trunk/model/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-	error_reporting(E_ALL | E_STRICT );
-	ini_set('display_errors', true);
-	
-	
-	mysql_connect("localhost","root","") or die(mysql_error());
-	mysql_select_db("iut") or die(mysql_error());
-	mysql_query("set names 'UTF8'");
-?>
