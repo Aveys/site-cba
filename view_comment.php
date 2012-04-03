fd8d617d678986d3f94fd199f2d8cf55b0d3fc63
thomas.rovayaz74@gmail.com <thomas.rovayaz74@gmail.com@03b04cd0-f2ea-658a-dc89-4b6bc1b6f185>
2012-04-03 15:44:41 +0000

    ajout de toutes les editions

--- view_comment.php
+++ view_comment.php
@@ -24,7 +24,7 @@ function add_commentaire($row,$text_button)
 				echo "<textarea name='commentaire' cols='50' row='30'></textarea></br>";
 				echo "<input type='submit' name='action' value='Commenter'/>";
 				echo "<input type='hidden' name='id' value='".$row["POST_ID"]."'/>";
-				echo "<input type='hidden' name='url' value='?page=article&POST_ID=".$row["post_id"]."'/>";
+				echo "<input type='hidden' name='url' value='?page=article&POST_ID=".$row["POST_ID"]."'/>";
 			echo"</form>";
 		}
 	}
@@ -38,9 +38,15 @@ function afficheCom($row)
 	while ($row_com = mysql_fetch_assoc($result_com)) {
 		if($row_com['com_parent'] == "")
 		{
-			echo nl2br($row_com['com_content'])." de ";
+			if(isset($_GET['edit']) && $row_com['com_id'] == $_GET['edit'])
+				affiche_form_edition(nl2br($row_com['com_content']),$row_com['com_id'],$row['POST_ID']);
+			else
+				echo nl2br($row_com['com_content']);
+			echo " de ";
 			link_profil($row_com['user_id']);		//apercu du profil de l'auteur du com
 			dateTimeToTime($row_com['com_date']);	//affiche la date de publication au format facebook
+			if(!(isset($_GET['edit']) && $row_com['com_id'] == $_GET['edit']))
+				button_edit_com($row_com['com_id'],$row['POST_ID']);	//bouton pour supprimer le com
 			button_delete_com($row_com['com_id'],$row['POST_ID']);	//bouton pour supprimer le com
 			add_commentaire($row_com,'▼');			//formulaire d'ajout de com à ce com
 			echo "<div id='comOfCom'>";	
@@ -56,9 +62,14 @@ function afficheComOfCom($id_com_parent)
 {
 	$result_com = sql_com_of_com_post_with_log($id_com_parent);
 	while ($row_com = mysql_fetch_assoc($result_com)) {
-		echo nl2br($row_com['com_content'])." de ";
+		if(isset($_GET['edit']) && $row_com['com_id'] == $_GET['edit'])
+			affiche_form_edition(nl2br($row_com['com_content']),$row_com['com_id'],$row_com['post_id']);
+		else
+			echo nl2br($row_com['com_content']);
+		echo " de ";
 		link_profil($row_com['user_id']);		//apercu du profil de l'auteur du com
 		dateTimeToTime($row_com['com_date']);	//affiche la date de publication au format facebook
+		button_edit_com($row_com['com_id'],$row_com['post_id']); //bouton de suppression de ce com
 		button_delete_com($row_com['com_id'],$row_com['post_id']); //bouton de suppression de ce com
 		echo "</br>";
 	}
@@ -69,14 +80,47 @@ function button_delete_com($idCom,$idPost)
 {
 	if(isset($_SESSION['id']))
 	{
-	if(isadmin($_SESSION['id']))
-	{
-		echo "<form name='delete_com' action='controller/actions.php' method='post'>";
-			echo "<input name='id_com' type='hidden' value='".$idCom."'/>";
-			echo "<input name='action' value='Supprimer com' type='submit'/>";
-			echo "<input type='hidden' name='url' value='?page=article&POST_ID=".$idPost."'/>";
-		echo "</form>";
+		if(isadmin($_SESSION['id']))
+		{
+			echo "<form name='delete_com' action='controller/actions.php' method='post'>";
+				echo "<input name='id_com' type='hidden' value='".$idCom."'/>";
+				echo "<input name='action' value='Supprimer com' type='submit'/>";
+				echo "<input type='hidden' name='url' value='?page=article&POST_ID=".$idPost."'/>";
+			echo "</form>";
+		}
 	}
+}
+/*	affiche un formulaire d'edition de com seulement si l'utilisateur est admin
+*/
+function affiche_form_edition($content,$idCom,$idPost)
+{
+	if(isset($_SESSION['id']))
+	{
+		if(isadmin($_SESSION['id']))
+		{
+			echo "<form name='edit_com' action='controller/actions.php' method='post'>";
+				echo "<textarea name='commentaire' cols='50' row='30'>".$content."</textarea></br>";
+				echo "<input name='id_com' type='hidden' value='".$idCom."'/>";
+				echo "<input name='action' value='Editer com' type='submit'/>";
+				echo "<input type='hidden' name='url' value='?page=article&POST_ID=".$idPost."'/>";
+			echo "</form>";
+		}
+	}
+}
+/*	affiche un formulaire d'edition de com seulement si l'utilisateur est admin
+*/
+function button_edit_com($idCom,$idPost)
+{
+	if(isset($_SESSION['id']))
+	{
+		if(isadmin($_SESSION['id']))
+		{
+			echo "<form name='edit_com_button' action='?page=article&POST_ID=".$idPost."&edit=".$idCom."' method='post'>";
+				echo "<input name='id_com' type='hidden' value='".$idCom."'/>";
+				echo "<input name='action' value='Editer com' type='submit'/>";
+				echo "<input type='hidden' name='url' value='?page=article&POST_ID=".$idPost."'/>";
+			echo "</form>";
+		}
 	}
 }
 ?>
