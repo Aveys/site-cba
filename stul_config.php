3faf4b9b9e628f7ce42da0382a28701f93daba71
mathieumartin743@gmail.com <mathieumartin743@gmail.com@03b04cd0-f2ea-658a-dc89-4b6bc1b6f185>
2012-04-03 06:59:54 +0000

    Modification du config.php

--- stul_config.php
+++ stul_config.php
@@ -1,16 +1,21 @@
 <?php
 //Ce fichier contient tout les variables globales que nous avons besoin
+if(!isset($ABSPATHCONFIG))
+	$ABSPATHCONFIG = dirname(__FILE__).'/';
 //Une variable qui commence par $f sera un fichier
 $vTheme			= 'themes/cba';
+
+
 
 $Site = 'site-cba';
 //Chemin des fichiers
 $fActionPhp 	= 'actions.php';
 $fAdminFonct 	= 'includes/admin/stul_fonctions.php';
 $fAdminAction	= 'includes/admin/stul_actions.php';
-$fAdmin 		= $_SERVER["DOCUMENT_ROOT"].'/'.$Site.'/stul_admin.php';
+$fAdmin 		= $ABSPATHCONFIG.'/stul_admin.php';
 $fLanguage		= 'includes/stul_language.php';
-$fConnect 		= $_SERVER["DOCUMENT_ROOT"].'/'.$Site.'/connect.php';
+$fConnect 		= $ABSPATHCONFIG.'/connect.php';
+$fViewerAdmin	= 'includes/admin/viewer/index.php';
 
 
 ?>
