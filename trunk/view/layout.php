5353707895ac5ac4948a7ff7d2501ee8c646e0cf
mathieumartin743@gmail.com <mathieumartin743@gmail.com@03b04cd0-f2ea-658a-dc89-4b6bc1b6f185>
2012-04-04 22:24:40 +0000

    Correction du placement des radio de recherche

--- trunk/view/layout.php
+++ trunk/view/layout.php
@@ -11,6 +11,7 @@ require_once($fcSearch);
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>CBA Website</title>
+	<link rel="stylesheet" type="text/css" href="themes/cba/commun.css" />
 	<link rel="stylesheet" type="text/css" href="themes/cba/layout.css" />
 	<script type="text/javascript" src="js/verification.js"></script>
 	<script type="text/javascript" src="js/calendrier.js"></script>
