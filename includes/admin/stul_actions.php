68a835556f1c92f079a8f6034088b042e8bfd208
mathieumartin743@gmail.com <mathieumartin743@gmail.com@03b04cd0-f2ea-658a-dc89-4b6bc1b6f185>
2012-04-04 19:09:53 +0000

    Correction de ajouter un article (tres lent avec la redirection en JS)

--- includes/admin/stul_actions.php
+++ includes/admin/stul_actions.php
@@ -47,29 +47,31 @@
 				break;
 
 			case 'Mettre à jour':
-				sql_edit_post($_POST) or die(mysql_error());
+				sql_edit_post($_POST);
 				echo '<script language="Javascript">document.location.replace("./viewer/index.php?mode=editArticles");</script>';
 			break;
 
 			case 'Mettre à jour le commentaire':
-				sql_edit_com($_POST) or die(mysql_error());
+				sql_edit_com($_POST);
 				echo '<script language="Javascript">document.location.replace("./viewer/index.php?mode=editComs");</script>';
 			break;
 
 			case 'Mettre à jour le compte':
-				sql_allEdit_user($_POST["id_user"], $_POST["login"], $_POST["password"], $_POST["pseudo"], $_POST["email"], $_POST["status"])or die(mysql_error());
+				sql_allEdit_user($_POST["id_user"], $_POST["login"], $_POST["password"], $_POST["pseudo"], $_POST["email"], $_POST["status"]);
 				echo '<script language="Javascript">document.location.replace("./viewer/index.php?mode=editComptes");</script>';
 			break;
 
 			case 'Ajouter ce compte':
-				sql_inscrire_user_by_admin($_POST["login"], $_POST["password"], $_POST["pseudo"], $_POST["email"], $_POST["dateReg"], $_POST["status"])or die(mysql_error());
+				sql_inscrire_user_by_admin($_POST["login"], $_POST["password"], $_POST["pseudo"], $_POST["email"], $_POST["dateReg"], $_POST["status"]);
 				//La redirection ce fera dans action, si oui ou non le login et valide
 				//echo '<script language="Javascript">document.location.replace("./viewer/index.php?mode=editComptes");</script>';
 			break;
-			case "Salut":
+			case "Ajouter article":
 				//print_r($_POST);
 				//addArticle($_POST['content'], $_SESSION["idUser"],$_POST['title'], $_POST['tags'], $_POST['category']) or die(mysql_error());	
-				addArticle($_POST['content'], $_SESSION["idUser"], $_POST['title'], $_POST['tags'], $_POST['category']) or die(mysql_error());				
+				//MatHack: Il faudra rajouter le champs de la categorie plus tard
+				//$_POST['category']
+				addArticle($_POST['content'], $_SESSION["idUser"], $_POST['title'], $_POST['tags'], 1);				
 				echo '<script language="Javascript">document.location.replace("./viewer/index.php?mode=editArticles");</script>';
 				//header('Location:./viewer/index.php?mode=editArticles');
 			break;
