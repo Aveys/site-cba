f611579b43f175426e2073e07178397cf1621c97
mathieumartin743@gmail.com <mathieumartin743@gmail.com@03b04cd0-f2ea-658a-dc89-4b6bc1b6f185>
2012-04-05 11:50:08 +0000

    retour rev 245

--- js/verification.js
+++ js/verification.js
@@ -1,44 +1,58 @@
-function validCompte(field)
-{
 
-	if(verifPseudo(field.pseudo) && verifMail(field.mail) && verifMdp(field.password))
-		return true;
-	else
-		return false;
-}
-
-function verifMdp(field)
-{
-	if(field.value == '')
-	{
+function verifTexte(field){
+	if(field.value==""){
+		field.className ="erreur";
 		return false;
 	}
-	else
-	{
-		return true;
+	else{
+		field.className ="valid";
+		return true;	
 	}
 }
-
-function verifMail(field)
-{
-	if(field.value == '')
-	{
+function verifPseudo(field){
+	if(field.value==""){
+		field.className ="erreur";
+		field.value = "Login";
 		return false;
 	}
-	else
-	{
-		return true;
+	else{
+		field.className ="valid";
+		return true;	
 	}
 }
-
-function verifPseudo(field)
-{
-	if(field.value == '')
-	{
+function modifPseudo(field){
+	field.value = "";
+}
+function verifMdp(field){
+	if(field.value == ""){
+		field.className ="erreur";
+		field.value = "Mots de passe";
+		field.type = "text";
 		return false;
 	}
-	else
-	{
-		return true;
+	else{
+		field.className ="valid";
+		return true;	
 	}
 }
+function modifMdp(field){
+	if(field.type == "text")
+		field.type = "password";
+	field.value = "";
+}
+
+function valid(){
+	var texteField=document.articles.texte; //on ne met pas le getElementById("ou"); car on n'a pas d'id="ou" dans notre formulaire.
+	var pseudoField=document.articles.pseudo; // idem, il faut id="nom" dans le forlmulaire.
+	var texteOK = verifTexte(texteField);
+	var pseudoOK = verifPseudo(pseudoField);
+	return texteOK && pseudoOK;
+}
+
+function validLogin(field){
+	var mdpField=field.mdp; //on ne met pas le getElementById("ou"); car on n'a pas d'id="ou" dans notre formulaire.
+	var pseudoField=field.pseudo; // idem, il faut id="nom" dans le forlmulaire.
+	return (verifPseudo(pseudoField) && verifMdp(mdpField));
+}
+
+//BLABLA kk
