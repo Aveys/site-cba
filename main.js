faf74fdaa9a5a94fe36fdd4c728cd585b5111478
nathanael.couret@gmail.com <nathanael.couret@gmail.com@03b04cd0-f2ea-658a-dc89-4b6bc1b6f185>
2012-04-05 11:45:56 +0000

    inscription.php

---
+++ main.js
@@ -0,0 +1,40 @@
+jQuery(function($){
+	
+	/** 
+   * Vérifications
+   * Vérification des champs
+   * */ 
+  $('input').blur(function(){
+    var input= $(this);
+    if(input.val() == '')
+    {
+       input.next('span').remove();
+       $('<span class="help-inline"/>').insertAfter(input).append('Le champ ne doit pas être vide !');
+    }
+    else
+    {
+      input.next('span').remove();
+    }
+  });
+	 
+	var domains = ['hotmail.com', 'gmail.com', 'wanadoo.fr', 'hotmail.fr', 'laposte.net', 'msn.com','etu.univ-savoie.fr', 'univ-savoie.fr'];
+	$('.mailcheck').blur(function(){
+		var input= $(this);
+		input.mailcheck({
+		domains: domains,
+		suggested: function(element, suggestion){
+			input.next('span').remove();
+			$('<span class="help-inline"/>').insertAfter(input).append('Oups ! Vouliez vous ecrire <a href="#">'+suggestion.full+'</a> ?').find('a').click(function(e){
+				e.preventDefault();
+				input.val($(this).text());
+				input.trigger('blur');
+			});
+		},
+		empty: function(element){
+			input.next('span').remove();
+		}
+		});
+	});
+	
+	
+});
